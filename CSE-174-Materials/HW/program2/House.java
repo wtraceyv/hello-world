3422000df0cd23d7bb7c290e5bdf6bd5ed91c04c
wtraceyv <wtraceyv@gmail.com>
2017-01-03 23:23:04 -0500

    Create House.java

---
+++ CSE-174-Materials/HW/program2/House.java
@@ -0,0 +1,22 @@
+/* Walter Tracey 
+* Instructor: Vajayalakshmi Ramasamy
+* CSE 174 C
+* 11 September 2016
+*/ 
+
+// Class to solve problem P1.9 in Big Java: 
+// print a house that looks exactly like that shown in the book 
+
+public class House{
+   public static void main(String[] args){
+      // just use some print statements and do some arty magic...: 
+      System.out.println("   +   "); 
+      System.out.println("  + +  "); 
+      System.out.println(" +   + "); 
+      System.out.println("+-----+"); 
+      System.out.println("| .-. |"); 
+      System.out.println("| | | |"); 
+      System.out.println("+-+-+-+");
+      // That looks pretty great... 
+   }
+}
