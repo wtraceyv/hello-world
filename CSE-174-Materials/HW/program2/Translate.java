5acecd1685670ab00349441eb939e6128595a07a
wtraceyv <wtraceyv@gmail.com>
2017-01-03 23:23:52 -0500

    Create Translate.java

---
+++ CSE-174-Materials/HW/program2/Translate.java
@@ -0,0 +1,19 @@
+/* Walter  Tracey 
+* Instructor: Vajayalakshmi Ramasamy
+* CSE 174 C
+* 11 September 2016
+*/ 
+
+// Class to solve problem P1.20 in Big Java: 
+// Print two columns of language greetings as shown 
+
+public class Translate{
+   public static void main(String[] args){
+      System.out.println("English                       French"); 
+      System.out.println("-------------------------------------------"); 
+      System.out.println("Good Morning                  Bonjour"); 
+      System.out.println("It is a pleasure to meet you  Enchant�"); 
+      System.out.println("Please call me tomorrow       S'il vous pla�t appelez-moi demain");
+      System.out.println("Have a nice day               Bonne Journ�e");
+   }
+}
