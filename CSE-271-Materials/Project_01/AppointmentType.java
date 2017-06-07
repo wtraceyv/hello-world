760a9638ae0aea10adf78d74c027ac98c03bb835
wtraceyv <wtraceyv@gmail.com>
2017-06-07 13:22:39 -0400

    Create AppointmentType.java

---
+++ CSE-271-Materials/Project_01/AppointmentType.java
@@ -0,0 +1,13 @@
+package project_one_src;
+
+/**
+ * enum to represent AppointmentTypes so that they 
+ * can be used as parameters elsewhere 
+ * CSE 271, B
+ * @author Wallisan
+ * Instructor: Dr. Stephan 
+ * 3/10/17
+ */
+public enum AppointmentType {
+	ONETIME, DAILY, MONTHLY
+}
