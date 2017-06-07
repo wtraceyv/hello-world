a9d4f2f00c9f896c73c0f8adde0a45cfb04eb022
wtraceyv <wtraceyv@gmail.com>
2017-06-07 12:39:10 -0400

    Create Movable.java

---
+++ CSE-271-Materials/Lab_06/Movable.java
@@ -0,0 +1,16 @@
+package lab_six_src;
+
+/**
+ * Interface to apply to objects who can move in 
+ * any of the four basic x-y based directions 
+ * @author Wallisan
+ * Instructor: Dr. Stephan 
+ * CSE 271, B
+ * 3/3/2017
+ */
+public interface Movable {
+	void moveUp(); 
+	void moveDown(); 
+	void moveLeft(); 
+	void moveRight(); 
+}// end Movable interface 
